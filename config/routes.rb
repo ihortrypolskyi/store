1382c6b02e7962a187bc5aa63df48d975247eb60
ihortrypolskyi <ihor.trypolskyi@gmail.com>
2017-01-05 00:21:08 +0200

    controllers, navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,69 @@
 Rails.application.routes.draw do
+  root 'books#index'
+
+  get 'publishers/new'
+
+  get 'publishers/create'
+
+  get 'publishers/update'
+
+  get 'publishers/edit'
+
+  get 'publishers/destroy'
+
+  get 'publishers/show'
+
+  get 'publishers/index'
+
+  get 'authors/new'
+
+  get 'authors/create'
+
+  get 'authors/update'
+
+  get 'authors/edit'
+
+  get 'authors/destroy'
+
+  get 'authors/show'
+
+  get 'authors/index'
+
+  get 'categories/new'
+
+  get 'categories/create'
+
+  get 'categories/update'
+
+  get 'categories/edit'
+
+  get 'categories/destroy'
+
+  get 'categories/show'
+
+  get 'categories/index'
+
+  get 'books/new'
+
+  get 'books/create'
+
+  get 'books/update'
+
+  get 'books/edit'
+
+  get 'books/destroy'
+
+  get 'books/show'
+
+  get 'books/index'
+
+  get 'about/index'
+
+  resources :books
+  resources :categories
+  resources :authors
+  resources :publishers
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
