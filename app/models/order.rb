e36adf24b5a727918cfdfb62a5a2eadd8d7fbc05
ihortrypolskyi <ihor.trypolskyi@gmail.com>
2017-01-20 19:05:29 +0200

    cart styling

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,10 +3,19 @@ class Order < ApplicationRecord
   has_many :order_books
   before_create :set_order_status
   before_save :update_subtotal
+  before_save :update_total
 
-  def subtotal
-    order_books.collect { |oi| oi.valid? ? (oi.quantity * oi.unit_price) : 0 }.sum
+  def order_subtotal
+
   end
+
+  def order_total
+    order_books.collect { |ob| ob.valid? ? (ob.quantity * ob.unit_price) : 0 }.sum
+  end
+
+  # def subtotal
+  #   order_books.collect { |ob| ob.valid? ? (ob.quantity * ob.unit_price) : 0 }.sum
+  # end
 
   private
 
@@ -16,6 +25,10 @@ class Order < ApplicationRecord
 
   def update_subtotal
     self[:subtotal] = subtotal
+  end
+
+  def update_total
+    self[:total] = total
   end
 end
 
