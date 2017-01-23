f68a87a40617b46f3ae9155715d0a26bbd28ad66
ihortrypolskyi <ihor.trypolskyi@gmail.com>
2017-01-23 22:21:57 +0200

    quantity, price updates for cart, js

--- app/controllers/order_books_controller.rb
+++ app/controllers/order_books_controller.rb
@@ -2,6 +2,7 @@ class OrderBooksController < ApplicationController
   def create
     @order = current_order
     @order_book = @order.order_books.new(order_book_params)
+
     @order.save
     session[:order_id] = @order.id
   end
@@ -11,6 +12,14 @@ class OrderBooksController < ApplicationController
     @order_book = @order.order_books.find(params[:id])
     @order_book.update_attributes(order_book_params)
     @order_books = @order.order_books
+    respond_to do |format|
+      if @order_book.save
+        format.js
+      else
+        format.html { redirect :back }
+      end
+    end
+
   end
 
   def destroy
