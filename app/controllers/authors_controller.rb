ce15e0d6cffe8c8f210e946850969d0329bc0c8f
ihortrypolskyi <ihor.trypolskyi@gmail.com>
2017-01-25 12:03:44 +0200

    reviews rating refactoring

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -24,6 +24,16 @@ class AuthorsController < ApplicationController
     @category = Category.find(params[:id])
     @order_book = current_order.order_books.new
 
+    for singlebook in @books
+      @reviews = Review.where(book_id: singlebook.id)
+
+      if @reviews.blank?
+        singlebook.avg_review = 0
+      else
+        singlebook.avg_review = @reviews.average(:rating).round(2)
+      end
+    end
+
   end
 
   def index
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -21,11 +21,20 @@ class CategoriesController < ApplicationController
     @book = Book.find(params[:id])
     @authors = Author.order(:first_name)
     @order_book = current_order.order_books.new
+
+    for singlebook in @books
+      @reviews = Review.where(book_id: singlebook.id)
+
+      if @reviews.blank?
+        singlebook.avg_review = 0
+      else
+        singlebook.avg_review = @reviews.average(:rating).round(2)
+      end
+    end
   end
 
   def index
     @categories = Category.order(:name)
-
   end
 
   private
