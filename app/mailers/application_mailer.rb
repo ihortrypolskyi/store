1ad4e39779977e357d5f84ae3e04af73e13550ca
ihortrypolskyi <ihor.trypolskyi@gmail.com>
2017-02-03 22:37:14 +0200

    0 reviews

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  ADMIN_EMAIL = "booksssstore@gmail.com".freeze
+  ADMIN_EMAIL = 'booksssstore@gmail.com'.freeze
   default from: 'booksssstore@gmail.com'
   layout 'mailer'
 end
