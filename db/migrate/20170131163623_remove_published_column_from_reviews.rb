6ef88d35a17b8aa02c349054e1de415dd2515eca
ihortrypolskyi <ihor.trypolskyi@gmail.com>
2017-02-08 22:35:51 +0200

    column published

--- db/migrate/20170131163623_remove_published_column_from_reviews.rb
+++ db/migrate/20170131163623_remove_published_column_from_reviews.rb
@@ -1,5 +1,5 @@
 class RemovePublishedColumnFromReviews < ActiveRecord::Migration[5.0]
   def change
-    remove_column :reviews, :published, :boolean
+    # remove_column :reviews, :published, :boolean
   end
 end
