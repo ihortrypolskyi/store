e72ea63200763aacc4417ba0dc277c600574b194
ihortrypolskyi <ihor.trypolskyi@gmail.com>
2017-02-14 19:03:02 +0200

    before s3 set

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,10 @@
-CarrierWave.configure do |config|
-  config.dropbox_app_key = 'dn7hmv36lur31zq'
-  config.dropbox_app_secret = 'rmypdwubi7biisj'
-  config.dropbox_access_token = 'js9lmun9hhxsu03u'
-  config.dropbox_access_token_secret = 'lrx4pnw8r1f8r4m'
-  config.dropbox_user_id = '595207289'
-  config.dropbox_access_type = 'app_folder'
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_secret_key_id: 'AKIAI445DIC7QZOYAT7Q',
+      aws_secret_access_key: '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'
+    }
+    config.fog_directory = 'booksssstore'
+  end
 end
