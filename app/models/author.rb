0ef6e5e791906ef3c205a10b2ef36c7cbcc3f86e
ihortrypolskyi <ihor.trypolskyi@gmail.com>
2017-02-14 23:07:15 +0200

    authors validation

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,6 +1,6 @@
 class Author < ApplicationRecord
   has_and_belongs_to_many :books
 
-  validates :author_first_name, presence: true, length: { in: 2..50 }
-  validates :author_last_name, presence: true, length: { in: 2..50 }
+  validates :first_name, presence: true, length: { in: 2..50 }
+  validates :last_name, presence: true, length: { in: 2..50 }
 end
