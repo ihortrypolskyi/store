155cb4521336a221ae6e79af69d6bc7dec451e96
ihortrypolskyi <ihor.trypolskyi@gmail.com>
2017-08-19 11:39:12 +0300

    create_author migration corrected

--- db/migrate/20170108093215_create_authors_table.rb
+++ db/migrate/20170108093215_create_authors_table.rb
@@ -7,3 +7,14 @@ class CreateAuthors < ActiveRecord::Migration[5.0]
     end
   end
 end
+
+# class CreateAuthors < ActiveRecord::Migration[5.0]
+#   def change
+#     create_table :authors do |t|
+#       t.string :author_first_name
+#       t.string :author_last_name
+
+#       t.timestamps
+#     end
+#   end
+# end
